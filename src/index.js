dcf87f63cbc71ea5b5d74c369cac32f3febe7729
Johnson Fu <johnson.fu@chubb.com>
2019-02-14 16:34:06 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// default webpack take this file as the entry point
+
+import App from "./App";
+
+console.log(`this is entry point.`);
+
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
+
